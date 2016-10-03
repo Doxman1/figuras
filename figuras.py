dbcdc6f2eacf90a1d3925f9e4a5a95120d61ce53
Gerardo González <jugerardo@hotmail.com>
2016-10-03 11:11:59 -0500

    Agrega configuración para CI Travis
    Se agrega el archivo .travis.yml, requerimientos.txt para poder
    realizar CI en Travis-ci.org

--- figuras.py
+++ figuras.py
@@ -1,30 +1,31 @@
 import math
 
+
 class Figuras:
-	
-	def rectangulo(self,base,altura):
-		try:
-			base = float(base)
-			altura = float(altura)
-			resultado = base*altura
-			return resultado
-		except Exception, e:
-			return 'dato incorrecto'
 
-	def triangulo(self,base,altura):
-		try:
-			base = float(base)
-			altura = float(altura)
-			resultado = (base*altura)/2
-			return resultado
-		except Exception, e:
-			return 'dato incorrecto'
+    def rectangulo(self, base, altura):
+        try:
+            base = float(base)
+            altura = float(altura)
+            resultado = base * altura
+            return resultado
+        except Exception, e:
+            return 'dato incorrecto'
 
-	def circunferencia(self,radio):
-		try:
-			radio = float(radio)
-			resultado = math.pi*(radio**2)
-			residuo = resultado % 0.01
-			return resultado - residuo
-		except Exception, e:
-			return 'dato incorrecto'
+    def triangulo(self, base, altura):
+        try:
+            base = float(base)
+            altura = float(altura)
+            resultado = (base * altura) / 2
+            return resultado
+        except Exception, e:
+            return 'dato incorrecto'
+
+    def circunferencia(self, radio):
+        try:
+            radio = float(radio)
+            resultado = math.pi * (radio**2)
+            residuo = resultado % 0.01
+            return resultado - residuo
+        except Exception, e:
+            return 'dato incorrecto'
